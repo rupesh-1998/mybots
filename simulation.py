96bf857c3420184745c0e4a0bf01e0bfd474f5cf
rupesh-1998 <rupesh.1998@hotmail.co.uk>
2021-12-05 17:42:17 +0000

    starting work on simulation

--- simulation.py
+++ simulation.py
@@ -1,2 +1,5 @@
 import pybullet as p
 
+#This creates an object, physicsClient, which handles the physics, and draws the results to a Graphical User Interface (GUI).
+physicsClient = p.connect(p.GUI)
+p.disconnect()
