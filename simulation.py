902986323db905955343bba2269f6f2ddb460775
rupesh-1998 <rupesh.1998@hotmail.co.uk>
2021-12-05 18:11:38 +0000

    starting onelink

--- simulation.py
+++ simulation.py
@@ -1,5 +1,13 @@
 import pybullet as p
+import time
 
 #This creates an object, physicsClient, which handles the physics, and draws the results to a Graphical User Interface (GUI).
 physicsClient = p.connect(p.GUI)
+
+#This statement increases the physics inside the world for a small amount. 
+#For example, if an object is suspended above the ground, 
+#it will move a small distance toward the ground, because simulated gravity is pulling it down.
+for i in range(1000):
+    p.stepSimulation()
+    time.sleep(1/60)
 p.disconnect()
