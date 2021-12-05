155a691c9f3255866621769b57d4b72f2a3661f3
rupesh-1998 <rupesh.1998@hotmail.co.uk>
2021-12-05 22:58:46 +0000

    saving sensor vals

--- simulation.py
+++ simulation.py
@@ -1,6 +1,8 @@
 import pybullet as p
 import time
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
+import numpy as np
 
 #This creates an object, physicsClient, which handles the physics, 
 #and draws the results to a Graphical User Interface (GUI).
@@ -22,7 +24,15 @@ p.loadSDF("min_wrld.sdf")
 #For example, if an object is suspended above the ground, 
 #it will move a small distance toward the ground, 
 #because simulated gravity is pulling it down.
+backLegSensorValues = np.zeros(10000)
+
+pyrosim.Prepare_To_Simulate("body.urdf")
 for i in range(1000):
     p.stepSimulation()
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("bLeg")
+
     time.sleep(1/60)
 p.disconnect()
+
+print(backLegSensorValues)
+np.save(r"C:\Users\rupes\Data Science Masters\Nature Inspired Computation\CW2\data", backLegSensorValues)
