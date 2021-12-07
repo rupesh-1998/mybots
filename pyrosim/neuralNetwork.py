19efd98e155425518856e5b4cb7780a1e1832356
rupesh-1998 <rupesh.1998@hotmail.co.uk>
2021-12-07 17:55:30 +0000

    neurons complete

--- pyrosim/neuralNetwork.py
+++ pyrosim/neuralNetwork.py
@@ -32,8 +32,22 @@ class NEURAL_NETWORK:
         for i in self.neurons:
             if(self.neurons[i].Is_Sensor_Neuron()):
                 self.neurons[i].Update_Sensor_Neuron()
-            #else:
-                #self.neurons[i].Update_Hidden_Or_Motor_Neuron()
+            else:
+                self.neurons[i].Update_Hidden_Or_Motor_Neuron()
+
+    
+    def Get_Neuron_Names(self):
+        return self.neurons.keys()
+
+    def Is_Motor_Neuron(self, neuron):
+        return self.neurons[neuron].Is_Motor_Neuron()
+
+    def Get_Motor_Neurons_Joint(self, neuron):
+        return self.neurons[neuron].Get_Joint_Name()
+
+    def Get_Value_Of(self, neuronName):
+        return self.neurons[neuronName].Get_Value()
+
 # ---------------- Private methods --------------------------------------
 
     def Add_Neuron_According_To(self,line):
--- robot.py
+++ robot.py
@@ -30,8 +30,12 @@ class ROBOT:
             self.motors[jointName] = MOTOR(jointName)
 
     def Act(self, t):
-         for i in self.motors:
-            self.motors[i].Set_Value(self.robot, t)
+        for neuronName in self.nn.Get_Neuron_Names():
+            if self.nn.Is_Motor_Neuron(neuronName):
+                jointName = self.nn.Get_Motor_Neurons_Joint(neuronName)
+                desiredAngle = self.nn.Get_Value_Of(neuronName)
+                self.motors[jointName].Set_Value(self.robot, desiredAngle)
+
 
     def Think(self):
         self.nn.Update()
